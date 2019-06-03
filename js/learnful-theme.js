83e985ae09edc2a25c050b2e1171f01f5ba5ea46
wlu_ydahi <ydahi@wlu.ca>
2019-06-03 15:35:16 -0400

    initial commit

---
+++ js/learnful-theme.js
@@ -0,0 +1,13 @@
+(function ($, Drupal, drupalSettings) {
+
+    'use strict';
+  
+    Drupal.behaviors.visualScripts = {
+      attach: function (context, settings) {
+
+        console.log('learnful-theme.js loaded and running.')
+
+      }
+    }
+
+})(jQuery, Drupal, drupalSettings);
