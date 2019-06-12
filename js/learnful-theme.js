b64632cfecbfecd7a95fbdfaf9b94948c0fcd382
ydahi <ymdahi@gmail.com>
2019-06-12 14:24:16 -0400

    styled offcanvas-right

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -11,17 +11,20 @@
         $("a#openUserNav").click(function() {
           $("aside.sidebar-offcanvas-right").animate({ width: "16%"});
           $("#full-page-overlay").css('display','block');
+          $("button#close-sidebar-off-right").css('display','block');
         });
 
         // close .sidebar-offcanvs-right
         $("button#close-sidebar-off-right").click(function() {
           $("aside.sidebar-offcanvas-right").animate({ width: "0px"});
           $("#full-page-overlay").css('display','none');
+          $("button#close-sidebar-off-right").css('display','none');
         });
 
         $("#full-page-overlay").click(function() {
           $("aside.sidebar-offcanvas-right").animate({ width: "0px"});
           $("#full-page-overlay").css('display','none');
+          $("button#close-sidebar-off-right").css('display','none');
         });
       }
     }
