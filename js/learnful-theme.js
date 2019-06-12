ceab158072a28a47a9a8371195b483f390587388
ydahi <ymdahi@gmail.com>
2019-06-12 13:35:29 -0400

    add trigger for closing offcanv-right

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -10,13 +10,13 @@
         // toggle .sidebar-offcanvas-right
         $("a#openUserNav").click(function() {
           $("aside.sidebar-offcanvas-right").animate({ width: "16%"});
-          $("#full-page-overlay").toggle();
+          $("#full-page-overlay").css('display','block');
         });
 
         // close .sidebar-offcanvs-right
         $("a#close-sidebar-off-right").click(function() {
           $("aside.sidebar-offcanvas-right").animate({ width: "0px"});
-          $("#full-page-overlay").toggle();
+          $("#full-page-overlay").css('display','none');
         });
 
       }
