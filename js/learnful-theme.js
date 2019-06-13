9f8658c386a497c45581486171dc74697ccfc616
ydahi <ymdahi@gmail.com>
2019-06-13 14:34:55 -0400

    changed offcanvas-right to use left position to animate instead of width

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -9,19 +9,19 @@
 
         // open .sidebar-offcanvas-right when menu is clicked
         $("a#openUserNav").click(function() {
-          $("aside.sidebar-offcanvas-right").addClass("is-open").animate({ width: "16%"}, 100);
+          $("aside.sidebar-offcanvas-right").addClass("is-open").animate({ right: "0px"}, 100);
           $("#full-page-overlay").css('display','block');
           $("button#close-sidebar-off-right").css('display','block');
         });
         // close .sidebar-offcanvs-right when close button is clicked
         $("button#close-sidebar-off-right").click(function() {
-          $("aside.sidebar-offcanvas-right").removeClass("is-open").animate({ width: "0px"}, 100);
+          $("aside.sidebar-offcanvas-right").removeClass("is-open").animate({ right: "-250px"}, 100);
           $("#full-page-overlay").css('display','none');
           $("button#close-sidebar-off-right").css('display','none');
         });
         // close .sidebar-offcanvs-right and navbar.collapse when body overlay is clicked
         $("#full-page-overlay").click(function() {
-          $("aside.sidebar-offcanvas-right").removeClass("is-open").animate({ width: "0px"}, 100);
+          $("aside.sidebar-offcanvas-right").removeClass("is-open").animate({ right: "-250px"}, 100);
           $("aside.sidebar-left").removeClass("is-open").animate({ left: "-250px"}, 100);
           // $("header#navbar #navbar-collapse").toggleClass("collapse");
           // $("header#navbar").css("z-index","1030");
