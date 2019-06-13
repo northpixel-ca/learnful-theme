8a2796a6b98fc5e991019446bcb42748e0c4eb75
ydahi <ymdahi@gmail.com>
2019-06-13 12:19:26 -0400

    style and script main nav mobile toggle

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -7,25 +7,33 @@
 
         //console.log('learnful-theme.js loaded and running.')
 
-        // toggle .sidebar-offcanvas-right
+        // open .sidebar-offcanvas-right when menu is clicked
         $("a#openUserNav").click(function() {
           $("aside.sidebar-offcanvas-right").addClass("is-open").animate({ width: "16%"}, 100);
           $("#full-page-overlay").css('display','block');
           $("button#close-sidebar-off-right").css('display','block');
         });
 
-        // close .sidebar-offcanvs-right
+        // close .sidebar-offcanvs-right when close button is clicked
         $("button#close-sidebar-off-right").click(function() {
           $("aside.sidebar-offcanvas-right").removeClass("is-open").animate({ width: "0px"}, 100);
           $("#full-page-overlay").css('display','none');
           $("button#close-sidebar-off-right").css('display','none');
         });
 
+        // close .sidebar-offcanvs-right when body overlay is clicked
         $("#full-page-overlay").click(function() {
           $("aside.sidebar-offcanvas-right").removeClass("is-open").animate({ width: "0px"}, 100);
           $("#full-page-overlay").css('display','none');
           $("button#close-sidebar-off-right").css('display','none');
         });
+
+        // open #navbar-collapse when menu is clicked
+        $("button#toggle-mobile-nav").click(function() {
+          $("header#navbar #navbar-collapse").toggleClass("collapse");
+        });
+
+
       }
     }
 
