12fb258d9c7c1a84d625f6f53f5bcc112e624dcc
ydahi <ymdahi@gmail.com>
2019-06-27 16:45:18 -0400

    add help block to entity browser widget

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -51,7 +51,9 @@
 
 
         // add help block to entity browser field
-        $('.field--widget-entity-browser-entity-reference .panel-body').prepend(`<p class="help-block">Select and organize the projects you want to display on your public profile.`);
+        $('.field--widget-entity-browser-entity-reference .panel-body').once(function(){
+          $(this).prepend(`<p class="help-block">Select and organize the projects you want to display on your public profile.`);
+        });
         
 
       }
