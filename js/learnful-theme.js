eaca5212b6df2c1f7458f94755bf23cbbe14b9ff
ydahi <ymdahi@gmail.com>
2019-07-08 16:29:59 -0400

    reverted non-animated events for menu closing/opening

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -7,26 +7,26 @@
 
         // open .sidebar-offcanvas-right when menu is clicked
         $("a#openUserNav").click(function() {
-          $("aside.sidebar-offcanvas-right").addClass("is-open").animate({ right: "0px"}, 100);
+          $("aside.sidebar-offcanvas-right").addClass("is-open").css("right", "0px");
           $("#full-page-overlay").css('display','block');
           $("button#close-sidebar-off-right").css('display','block');
         });
         // close .sidebar-offcanvs-right when close button is clicked
         $("button#close-sidebar-off-right").click(function() {
-          $("aside.sidebar-offcanvas-right").removeClass("is-open").animate({ right: "-250px"}, 100);
+          $("aside.sidebar-offcanvas-right").removeClass("is-open").css( "right", "-250px");
           $("#full-page-overlay").css('display','none');
           $("button#close-sidebar-off-right").css('display','none');
         });
         // close .sidebar-offcanvs-right and .sidebar-left when body overlay is clicked
         $("#full-page-overlay").click(function() {
-          $("aside.sidebar-offcanvas-right.is-open").removeClass("is-open").animate({ right: "-250px"}, 100);
-          $("aside.sidebar-left.is-open").removeClass("is-open").animate({ left: "-250px"}, 100);
+          $("aside.sidebar-offcanvas-right.is-open").removeClass("is-open").css("right","-250px");
+          $("aside.sidebar-left.is-open").removeClass("is-open").css("left", "-250px");
           $("#full-page-overlay").css('display','none');
           $("button#close-sidebar-off-right").css('display','none');
         });
         // open #navbar-collapse when menu is clicked
         $("button#toggle-mobile-nav").click(function() {
-          $("aside.sidebar-left").addClass("is-open").animate({ left: "0px"}, 100);
+          $("aside.sidebar-left").addClass("is-open").css("left", "0px");
           $("#full-page-overlay").css('display','block');
         });
 
