54f7356a9d8b5874588c350d8529b0ba60c0210a
ydahi <ymdahi@gmail.com>
2019-07-11 14:18:54 -0400

    styled h5p editor widget and form page

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -61,7 +61,7 @@
 
         // Move H5P Editor Settings to Settings Panel
         $('.field--widget-h5p-editor .panel-body > .form-type-checkbox').each(function(){
-          $(this).appendTo("#edit-group-h5p-settings > .panel-body");
+          $(this).prependTo("#edit-group-h5p-settings > .panel-body");
         })
 
       }
