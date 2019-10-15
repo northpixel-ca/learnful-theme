0652af1cce5ea3216ae370a64a0d8085b6f5034b
ydahi <ymdahi@gmail.com>
2019-10-15 16:50:26 -0400

    hook into ajax callback

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -126,22 +126,6 @@
           // jquery match height
           $(".card.card-grid-content .card-body").once().matchHeight();
           $(".match-height").once().matchHeight();
-
-          console.log('Ajax Success 0');
-        });
-
-        $(document).ready( function () {
-          $('body').bind('ajaxSuccess', function(data, status, xhr) {
-            console.log('Ajax Success 1');
-          });
-        });
-
-        $('input#edit-submit-resources', context).ajaxSuccess(function () {
-          console.log('Ajax Success 2');
-        });
-
-        $(".view-resources").ajaxComplete(function(event, XMLHttpRequest, ajaxOptions){
-          console.log('Ajax Success 3');
         });
 
       }
