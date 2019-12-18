9103ec569d05144110d0a950a4b4c5fb9cac356b
ydahi <ymdahi@gmail.com>
2019-12-18 14:45:44 -0500

    restyled launch page header

--- js/learnful-theme.js
+++ js/learnful-theme.js
@@ -128,6 +128,16 @@
           $(".match-height").once().matchHeight();
         });
 
+        // smooth scroll for launch page
+        $('a.smooth-scroll').on("click", function(e){
+          var targetEl = $(this).attr('data-scrollTarget');
+          e.preventDefault();
+          document.querySelector(targetEl).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start' //scroll to top of the target element
+          });
+        });
+
       }
     }
 
